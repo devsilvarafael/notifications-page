9c24e792560218c9b2aa90d7b0c2836eb7030520
Rafael Silva <devsilva.rafael@gmail.com>
2023-01-03 01:08:06 -0300

    feat(component): Created NotificationViewer component to display on screen all data using the Notification component.

---
+++ src/components/sections/NotificationViewer.jsx
@@ -0,0 +1,20 @@
+import { NotificationsHeader } from "../NotificationsHeader.jsx";
+import { Notification } from "../Notification.jsx";
+import { Fragment } from "react";
+import { useNotificationContext } from "../../contexts/NotificationContext.jsx";
+
+export const NotificationViewer = () => {
+    const { notifications, users } = useNotificationContext()
+
+    return (
+        <div>
+            <NotificationsHeader title={"Notifications"}/>
+            {notifications.length > 0 ? notifications.map((notification, index) => (
+                <Fragment key={notification.id}>
+                    <Notification user={users[index]} post={notification} markAsRead={notification}/>
+                </Fragment>
+
+            )) : <div>Não há notificações</div>}
+        </div>
+    )
+}
