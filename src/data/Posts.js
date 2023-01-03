f8f1a632e74840d91f6d103bab2c42045b66cee4
Rafael Silva <devsilva.rafael@gmail.com>
2023-01-03 16:53:45 -0300

    chore: Add picture property in Posts static data, and adjust app content width.

--- src/data/Posts.js
+++ src/data/Posts.js
@@ -1,40 +1,45 @@
+import CommentedPicture from "../assets/images/image-chess.webp"
+
 export const posts = [
     {
         id: 0,
-        action: "reacted to your recent post",
+        action: "reacted-post",
         description: "My first tournament today!",
         time: "1m ago"
     },
     {
         id: 1,
-        action: "followed you",
+        action: "follow",
         time: "5m ago"
     },
     {
         id: 2,
-        action: "has joined your group",
+        action: "joined-group",
         description: "Chess Club",
         time: "1 day ago"
     },
     {
         id: 3,
-        action: "sent you a private message",
+        action: "sent-message",
+        message: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and " +
+            "I'm already having lots of fun and improving my game.",
         time: "5 days ago"
     },
     {
         id: 4,
-        action: "comment in your picture",
+        action: "comment-picture",
+        picture: CommentedPicture,
         time: "1 week ago"
     },
     {
         id: 5,
-        action: "reacted to your recent post",
+        action: "reacted-post",
         description: "5 end-game strategies to increase your win rate",
         time: "2 weeks ago"
     },
     {
         id: 6,
-        action: "left the group",
+        action: "left-group",
         description: "Chess Club",
         time: "2 weeks ago"
     }
