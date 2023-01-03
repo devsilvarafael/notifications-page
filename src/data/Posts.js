8fac96161bf245e22319a4a212b38a86434640ee
Rafael Silva <devsilva.rafael@gmail.com>
2023-01-03 01:01:00 -0300

    feat(data): Created static data for testing.

---
+++ src/data/Posts.js
@@ -0,0 +1,41 @@
+export const posts = [
+    {
+        id: 0,
+        action: "reacted to your recent post",
+        description: "My first tournament today!",
+        time: "1m ago"
+    },
+    {
+        id: 1,
+        action: "followed you",
+        time: "5m ago"
+    },
+    {
+        id: 2,
+        action: "has joined your group",
+        description: "Chess Club",
+        time: "1 day ago"
+    },
+    {
+        id: 3,
+        action: "sent you a private message",
+        time: "5 days ago"
+    },
+    {
+        id: 4,
+        action: "comment in your picture",
+        time: "1 week ago"
+    },
+    {
+        id: 5,
+        action: "reacted to your recent post",
+        description: "5 end-game strategies to increase your win rate",
+        time: "2 weeks ago"
+    },
+    {
+        id: 6,
+        action: "left the group",
+        description: "Chess Club",
+        time: "2 weeks ago"
+    }
+]
---
+++ src/data/Users.js
@@ -0,0 +1,47 @@
+import MarkWebberAvatar from "../assets/images/avatar-mark-webber.webp";
+import AngelaGrayAvatar from "../assets/images/avatar-angela-gray.webp";
+import JacobThompsonAvatar from "../assets/images/avatar-jacob-thompson.webp";
+import NathanPetersonAvatar from "../assets/images/avatar-nathan-peterson.webp";
+import RizkyHasanuddinAvatar from "../assets/images/avatar-rizky-hasanuddin.webp";
+import KimberlySmithAvatar from "../assets/images/avatar-kimberly-smith.webp";
+import AnnaKimAvatar from "../assets/images/avatar-anna-kim.webp";
+
+export const usersList = [
+
+
+    {
+        id: Math.floor(Math.random() * 100),
+        name: "Mark Webber",
+        avatar: MarkWebberAvatar
+    },
+    {
+        id: Math.floor(Math.random() * 101),
+        name: "Angela Gray",
+        avatar: AngelaGrayAvatar
+    },
+    {
+        id: Math.floor(Math.random() * 102),
+        name: "Jacob Thompson",
+        avatar: JacobThompsonAvatar
+    },
+    {
+        id: Math.floor(Math.random() * 103),
+        name: "Rizky Hasanuddin",
+        avatar: RizkyHasanuddinAvatar
+    },
+    {
+        id: Math.floor(Math.random() * 108),
+        name: "Kimberly Smith",
+        avatar: KimberlySmithAvatar
+    },
+    {
+        id: Math.floor(Math.random() * 104),
+        name: "Nathan Peterson",
+        avatar: NathanPetersonAvatar
+    },
+    {
+        id: Math.floor(Math.random() * 105),
+        name: "Anna Kim",
+        avatar: AnnaKimAvatar
+    }
+]
