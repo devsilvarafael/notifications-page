0becff56b623db5b910674cc4895f935f08c340b
Rafael Silva <devsilva.rafael@gmail.com>
2023-01-03 16:52:23 -0300

    feat(component): Created component to check post type and show a message or a picture.

---
+++ src/components/ActionNotification.jsx
@@ -0,0 +1,19 @@
+import styles from "../styles/components/action-notification.module.css"
+
+export const ActionNotification = ({ children, type, picture }) => {
+    switch (type) {
+        case "sent-message" :
+            return (
+                <div className={styles.message}>
+                    {children}
+                </div>
+            )
+        case "comment-picture" :
+            return (
+                <div className={styles.pictureContainer}>
+                    <img src={picture} className={styles.picture}/>
+                </div>
+
+            )
+    }
+}
